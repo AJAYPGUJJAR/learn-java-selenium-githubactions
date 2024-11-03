4fb6dbee0675b5a0def0c8a52394afcd87c812b4
Ajay P Gujjar <ajaypgujjar@yahoo.com>
2024-11-03 20:33:01 +0530

    added chrome headless

--- src/main/java/com/apsg/uitests/utils/WebdriverFactory.java
+++ src/main/java/com/apsg/uitests/utils/WebdriverFactory.java
@@ -2,13 +2,16 @@ package com.apsg.uitests.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
 public class WebdriverFactory {
 
     public WebDriver getWebDriver() {
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
