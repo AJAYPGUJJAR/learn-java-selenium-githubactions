a1532fe7aac220731d6c177c9b3fef9cbd8ae99b
Ajay P Gujjar <ajaypgujjar@yahoo.com>
2024-11-03 21:58:42 +0530

    added Test Report

--- src/test/java/com/apsg/uitests/TestRunner.java
+++ src/test/java/com/apsg/uitests/TestRunner.java
@@ -5,7 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features/Sample.feature",
-        glue = "com.apsg.uitests.stepdefs"
+        glue = "com.apsg.uitests.stepdefs",
+        plugin = {"pretty", "html:target/results/cucumber-report.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
